f8c24132cf29dfe2c2d35ee0e928c6e5462e4822
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2023-06-09 19:10:24 -0400

    Add interaction to add button.

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Enter Todo")
-input_box = sg.InputText()
+input_box = sg.InputText(key='todo')
 btn_add = sg.Button("Add")
 btn_edit = sg.Button("Edit")
 btn_complete = sg.Button("Complete")
@@ -10,12 +10,26 @@ btn_exit = sg.Button("Exit")
 
 layout = [
     [label, input_box, btn_add],
-    [btn_edit, [btn_complete]],
+    [[btn_complete], btn_edit],
     [btn_exit]
 
 ]
 
-window = sg.Window("TODO", layout)
+window = sg.Window("TODO", layout, font=('Roboto', 20))
 
-window.read()
+while True:
+    event, values = window.read()
+    # print(event)
+    # print(values)
+
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.set_todos(todos)
+
+        case sg.WINDOW_CLOSED:
+            break
+
 window.close()
